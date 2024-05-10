356ce5125bccae2e8323de2b068e112a0e63894f
DewniVishadi <vishadiwimalaweera@gmail.com>
2024-05-10 14:28:05 +0530

    DB Configs for Room Library

---
+++ app/src/main/java/com/example/task_management_app/TaskRoomDatabase.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.task_management_app
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskRoomDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskRoomDatabase? = null
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): TaskRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskRoomDatabase::class.java,
+                    "task_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .addCallback(TaskDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+
+        private class TaskDatabaseCallback(
+            private val scope: CoroutineScope
+        ) : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                INSTANCE?.let { database ->
+                    scope.launch(Dispatchers.IO) {
+                        populateDatabase(database.taskDao())
+                    }
+                }
+            }
+        }
+
+        suspend fun populateDatabase(taskDao: TaskDao) {
+            taskDao.deleteAll()
+            var task = Task("Hello")
+            taskDao.insert(task)
+            task = Task("World!")
+            taskDao.insert(task)
+        }
+    }
+}
